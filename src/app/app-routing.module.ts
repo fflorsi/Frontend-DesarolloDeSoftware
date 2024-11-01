93b5d3bd06603e9a0eec7db452ffd2bd186a81de
FacundoMunne <k50361@frro.utn.edu.ar>
2024-11-01 12:47:16 -0300

    a ver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,14 @@ import { ViewPetComponent } from './view-pet/view-pet.component';
 import { ViewClientPetComponent } from './view-client-pet/view-client-pet.component';
 import {ViewAllPetComponent} from './view-all-pet/view-all-pet.component'
 import { ViewMedicalHistoriesComponent } from './view-medical-histories/view-medical-histories.component';
+import { LoginComponent } from './componentsLogin/login/login.component';
+import { SignInComponent } from './componentsLogin/sign-in/sign-in.component';
+import { DashboardComponent } from './componentsLogin/dashboard/dashboard.component';
+import { AuthGuard } from './utils/auth.guard';
+import { NavbarComponent } from './componentsLogin/navbar/navbar.component';
+import { ListClientsComponent } from './componentsClient/list-clients/list-clients.component';
+import { AddEditClientComponent } from './componentsClient/add-edit-client/add-edit-client.component';
+import { DetailClientComponent } from './componentsClient/detail-client/detail-client.component';
 
 
 
@@ -19,7 +27,17 @@ export const routes: Routes = [
     {path: 'ViewClientPet', component:ViewClientPetComponent},
     {path: 'ViewAllPet', component: ViewAllPetComponent},
     
-    {path: 'ViewMedicalHistory/:petId', component: ViewMedicalHistoriesComponent}
+    {path: 'ViewMedicalHistory/:petId', component: ViewMedicalHistoriesComponent},
+
+    {path: 'login',component: LoginComponent },
+    {path: 'signIn', component: SignInComponent},
+    {path: 'dashboard', component: DashboardComponent,canActivate: [AuthGuard]},
+    {path: 'navbar', component: NavbarComponent},
+    {path: 'listClients', component: ListClientsComponent},
+    {path: 'addClient', component: AddEditClientComponent},
+    {path: 'editClient/:id', component: AddEditClientComponent},
+    {path: 'detailClient/:id',component: DetailClientComponent},
+    {path: '**', redirectTo: 'Home', pathMatch:'full'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { ViewPetComponent } from './view-pet/view-pet.component';
 import { AddPetComponent } from './add-pet/add-pet.component';
 import { EditPetComponent } from './edit-pet/edit-pet.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -19,6 +19,16 @@ import { ViewAllPetComponent } from './view-all-pet/view-all-pet.component';
 import {ViewMedicalHistoriesComponent} from './view-medical-histories/view-medical-histories.component'
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
+import { LoginComponent } from 'app/componentsLogin/login/login.component';
+import { SignInComponent } from 'app/componentsLogin/sign-in/sign-in.component';
+import { DashboardComponent } from 'app/componentsLogin/dashboard/dashboard.component';
+import {NavbarComponent} from 'app/componentsLogin/navbar/navbar.component';
+import {SpinnerComponent} from 'app/shared/spinner/spinner.component';
+import {AddTokenInterceptor} from 'app/utils/add-token.interceptor';
+import { ListClientsComponent } from 'app/componentsClient/list-clients/list-clients.component';
+import { AddEditClientComponent } from 'app/componentsClient/add-edit-client/add-edit-client.component';
+import {ProgressBarComponent} from 'app/shared/progress-bar/progress-bar.component'
+import { DetailClientComponent } from 'app/componentsClient/detail-client/detail-client.component';
 
 
 @NgModule({
@@ -31,7 +41,18 @@ import { MatTableModule } from '@angular/material/table';
     ViewClientPetComponent,
     ClientDniModalComponent,
     ViewAllPetComponent,
-    ViewMedicalHistoriesComponent
+    ViewMedicalHistoriesComponent,
+    SignInComponent,
+    SpinnerComponent,
+    LoginComponent,
+    DashboardComponent,
+    NavbarComponent,
+    ClientDniModalComponent,
+    ListClientsComponent,
+    AddEditClientComponent,
+    ProgressBarComponent,
+    ListClientsComponent,
+    DetailClientComponent
   ],
   imports: [
     BrowserModule,
@@ -45,7 +66,8 @@ import { MatTableModule } from '@angular/material/table';
     MatPaginatorModule,
     MatTableModule
   ],
-  providers: [ ],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AddTokenInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
