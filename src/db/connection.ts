9429bc5650c3d42aab21cb97e719c5215c45fa50
FacundoMunne <k50361@frro.utn.edu.ar>
2024-11-01 12:50:09 -0300

    borre cosas que estaban de mas

--- src/db/connection.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-
-const sequelize = new Sequelize('veterinary', 'root', 'facundo9', {
-    host: 'localhost',
-    dialect: 'mysql',   
-});
-
-export default sequelize;
--- src/user/user.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router} from "express";
-import {loginUser, newUser} from './user.controler.js';
-
-export const userRouter = Router()
-
-userRouter.post('/',newUser)
-userRouter.post('/login',loginUser);
-
