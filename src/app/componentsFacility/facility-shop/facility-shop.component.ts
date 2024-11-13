b131e35fafe2d7c7c87e0bfa2a51496855051cd7
fflorsi <fdegiusti@frro.utn.edu.ar>
2024-11-13 19:40:43 -0300

    servicios para que el cliente vea lo que tenemos

--- src/app/componentsFacility/facility-shop/facility-shop.component.ts
+++ src/app/componentsFacility/facility-shop/facility-shop.component.ts
@@ -2,16 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { Facility } from '@app/interfaces/facility';
 import { FacilityService } from '@app/services/facility.service';
 import { ToastrService } from 'ngx-toastr';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-facility-shop',
-
   templateUrl: './facility-shop.component.html',
-  styleUrl: './facility-shop.component.scss'
+  styleUrls: ['./facility-shop.component.scss']
 })
 export class FacilityShopComponent implements OnInit {
   listFacilities: Facility[] = [];
+  paginatedFacilities: Facility[] = [];
   loading: boolean = false;
+  paginator = {
+    pageIndex: 0,
+    pageSize: 6
+  };
+  pageEvent?: PageEvent;
 
   constructor(private _facilityService: FacilityService, private toastr: ToastrService) { }
 
@@ -26,6 +32,20 @@ export class FacilityShopComponent implements OnInit {
       console.log('Respuesta del servidor:', response); // Para ver la estructura completa
       this.listFacilities = response.data; // Accede al array a través de response.data
       this.loading = false;
-    })
+      this.setPaginatedFacilities(); // Llama a setPaginatedFacilities después de actualizar listFacilities
+    });
+  }
+
+  setPaginatedFacilities() {
+    const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
+    const endIndex = startIndex + this.paginator.pageSize;
+    this.paginatedFacilities = this.listFacilities.slice(startIndex, endIndex);
+  }
+
+  handlePageEvent(event: PageEvent) {
+    this.pageEvent = event;
+    this.paginator.pageIndex = event.pageIndex;
+    this.paginator.pageSize = event.pageSize;
+    this.setPaginatedFacilities();
   }
 }
