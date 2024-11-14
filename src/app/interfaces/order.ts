154791c6387d83743d922ffcd45654c36c73ee89
FacundoMunne <k50361@frro.utn.edu.ar>
2024-11-14 18:32:03 -0300

    order inicio

---
+++ src/app/interfaces/order.ts
@@ -0,0 +1,15 @@
+export interface OrderItem {
+    id: number;
+    quantity: number;
+    price: number;
+    product?: {
+        id:number
+        name:string
+    } 
+  }
+  
+  export interface Order {
+    id: number;
+    total: number;
+    items: OrderItem[];
+  }
