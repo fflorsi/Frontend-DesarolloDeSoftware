7298bcef51be5e54e4688c29754c35a681e9d163
GeRo2004 <gerocolman82@gmail.com>
2024-11-15 20:31:16 -0300

    delete professional

--- src/app/componentsProfessional/delete-professional/delete-professional.component.ts
+++ src/app/componentsProfessional/delete-professional/delete-professional.component.ts
@@ -8,31 +8,7 @@ import {ClientService} from 'app/services/client.service'
   templateUrl: './delete-professional.component.html',
   styleUrl: './delete-professional.component.scss'
 })
-export class professionalList implements OnInit {
-   professionalList: Array<{ dni: string, lastname: string, name: string, adress: string, phone_number: string, mail: string, birthdate: string, id?: number}> = [];
-  loading: boolean=false;
-
-  constructor(private _clientService: ClientService, private toastr: ToastrService){}
-
+export class DeleteProfessionalComponent {  
   ngOnInit(): void {
-    this.getListClients();
-  }
-
-  getListClients(){
-    this.loading=true;
-
-    this._clientService.getClients().subscribe((response: any) => {
-      console.log('Respuesta del servidor:', response); // Para ver la estructura completa
-      //this.listClients = response.data; // Accede al array a través de response.data
-      this.loading = false;
-    })
-  }
-
-  deleteClient(id: number){
-    this.loading=true;
-    this._clientService.deleteClientById(id).subscribe(()=>{
-      this.getListClients();
-      this.toastr.warning('El cliente fue eliminado con éxito','Cliente eliminado');
-    })
   }
 }
