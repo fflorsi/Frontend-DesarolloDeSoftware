e5ab408f341a1893856771614f11f2f4a9576477
David <davidcanallon08@gmail.com>
2024-11-15 18:03:56 -0300

    Fixed unlink vaccine

--- src/app/componentsMedicalHistory/view-medical-histories/view-medical-histories.component.ts
+++ src/app/componentsMedicalHistory/view-medical-histories/view-medical-histories.component.ts
@@ -238,6 +238,18 @@ openDeleteObservationDialog(observation: any): void {
     }
   });
 }
+
+unlinkVaccine(medicalHistoryId: number, vaccineId: number): void {
+  this.httpProvider.unlinkVaccine(medicalHistoryId, vaccineId).subscribe({
+    next: (response: any) => {
+      console.log('Vaccine unlinked successfully:', response);
+      this.getMedicalHistories();
+    },
+    error: (error) => {
+      console.error('Error unlinking vaccine:', error);
+    }
+  });
+}
 }
 
 
